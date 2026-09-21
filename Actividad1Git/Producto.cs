0660b77335e00c67d5776d3a5ad2a7ef27e3d0d7
Angel Rodenas <angelrodenasrizo@gmail.com>
2026-09-21 20:07:14 +0200

    Crear la clase Producto con atributos básicos (ej. nombre, precio).

---
+++ Actividad1Git/Producto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actividad1Git
+{ 
+    public class Producto
+    {
+        private string _nombre;
+        private int _precio;
+    }
+}
