c44bbe0c9a945d1378e7d47764e66a7198304bb3
Marcos Martinez Gimenez <marmargim5@alu.edu.gva.es>
2026-09-21 20:17:23 +0200

    Crear la clase Persona con atributos básicos (ej. nombre, edad)

---
+++ Actividad1Git/Persona.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Actividad1Git
+{
+    public class Persona
+    {
+        private string _nombre;
+        private string _edad;
+    }
+}
