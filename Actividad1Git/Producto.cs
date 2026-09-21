aa638f53aa4caad96906fdc28179774bcbea152d
Angel Rodenas <angelrodenasrizo@gmail.com>
2026-09-21 20:11:19 +0200

    Crear el constructor

--- Actividad1Git/Producto.cs
+++ Actividad1Git/Producto.cs
@@ -6,7 +6,34 @@ namespace Actividad1Git
 { 
     public class Producto
     {
+
         private string _nombre;
         private int _precio;
+
+        public Producto(string nombre, int precio)
+        {
+            _nombre = nombre;
+            _precio = precio;
+        }
+
+        public string GetNombre(string nombre) 
+        {
+            return _nombre;
+        }
+
+        public void SetNombre(string nombre) 
+        {
+            _nombre = nombre;
+        }
+
+        public int GetPrecio()
+        {
+            return _precio;
+        }
+
+        public void SetPrecio(int precio) { 
+        
+            _precio = precio;
+        }
     }
 }
